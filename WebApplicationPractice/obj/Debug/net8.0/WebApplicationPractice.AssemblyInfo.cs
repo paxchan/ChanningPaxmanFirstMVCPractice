42ecbbe7d8295f40b5243647db7afa7345cc3851
Channing Paxman <channing.paxman@gmail.com>
2025-02-04 20:35:36 -0700

    comments

--- WebApplicationPractice/obj/Debug/net8.0/WebApplicationPractice.AssemblyInfo.cs
+++ WebApplicationPractice/obj/Debug/net8.0/WebApplicationPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17526d1e743e9b5f08d842e78dab6c9d44d0d18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
